8fe7d636c65ec94f55be0403b482adc39788c23e
Maria Campbell <interglobalmedia@gmail.com>
2026-06-30 10:25:23 -0400

    Add path import, test and resolve configs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite';
+import path from 'path'
 
 export default defineConfig({
   base: './',
   build: {
     outDir: 'dist',
+  },
+  test: {
+    setupFiles: ['vitest-localstorage-mock'],
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      // provider: 'istanbul',
+      provider: 'v8',
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@modules': path.resolve(__dirname, './src/modules'),
+    },
   },
 });
